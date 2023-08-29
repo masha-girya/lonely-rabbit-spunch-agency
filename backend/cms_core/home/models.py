bfdb14e41a481f1485ef5c9092991a82513dfe2b
m00nc87 <i.mikulenko@dudka.agency>
2023-08-29 17:25:52 +0300

    [+] some

--- backend/cms_core/home/models.py
+++ backend/cms_core/home/models.py
@@ -1,7 +1,16 @@
 from django.db import models
-
+from wagtail.admin.panels import FieldPanel
 from wagtail.models import Page
+from wagtail.api import APIField
 
 
 class HomePage(Page):
-    pass
+    first_block_title = models.CharField(max_length=27, default='')
+
+    content_panels = Page.content_panels + [
+        FieldPanel('first_block_title'),
+    ]
+
+    api_fields = [
+        APIField('first_block_title'),
+    ]
