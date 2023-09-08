6c3d0ffc3f44f3b7cb1d32e7ee371ea9f5f823ce
Masha <teenderrr@gmail.com>
2023-09-08 16:48:36 +0300

    [*] qa fixes

--- frontend/src/components/modals-ui/vacancy-apply/index.tsx
+++ frontend/src/components/modals-ui/vacancy-apply/index.tsx
@@ -85,14 +85,14 @@ export const VacancyApplyModal: React.FC<IVacancyApplyModal> = (props) => {
               darkMode
               errorText={errors.name}
               value={name}
-              placeholder="Full Name"
+              placeholder="Full name"
               onChange={setName}
             />
             <Input
               darkMode
               errorText={errors.email}
               value={email}
-              placeholder="Email"
+              placeholder="E-mail"
               onChange={setEmail}
             />
             <Input
@@ -100,7 +100,7 @@ export const VacancyApplyModal: React.FC<IVacancyApplyModal> = (props) => {
               errorText={errors.coverLetter}
               type="textarea"
               value={coverLetter}
-              placeholder="Cover Letter"
+              placeholder="Cover letter"
               onChange={setCoverLetter}
             />
             <div className={styles.apply__rightCol__button}>
