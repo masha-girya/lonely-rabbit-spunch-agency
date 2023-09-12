0dc7ba8af432ef21dc7e77cc732667477258ff90
Masha <teenderrr@gmail.com>
2023-09-12 10:22:24 +0300

    [*] fixing

--- frontend/src/components/characters/index.tsx
+++ frontend/src/components/characters/index.tsx
@@ -19,7 +19,7 @@ export const Characters = () => {
       setCurrentSlide(
         CHARACTERS_MOCK[Math.floor(CHARACTERS_MOCK.length / 2)].charId
       );
-    }, 10)
+    }, 1)
   }, [CHARACTERS_MOCK]);
 
   return (
