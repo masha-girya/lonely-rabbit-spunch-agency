d0c7ee1473ffd836f1ab03434d4d29993fa82f96
Masha <teenderrr@gmail.com>
2023-09-14 11:04:17 +0300

    [*] fixed

--- frontend/pages/vacancies/index.tsx
+++ frontend/pages/vacancies/index.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useState } from "react";
+import { useState } from "react";
 import { Header } from "@components/header";
 import { Footer } from "@components/footer";
 import { VacancyCard } from "@components/vacancy-card";
